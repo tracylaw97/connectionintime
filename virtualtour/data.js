065d0fd2e3d348681a1e96e97e77cee648c06cd1
Tracy Law <tlaw.architecture@gmail.com>
2020-08-15 09:21:00 -0700

    final

---
+++ virtualtour/data.js
@@ -0,0 +1,582 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-past-pod",
+      "name": "Past Pod",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5565958507348503,
+        "pitch": 0.020965821314653965,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7626556975453678,
+          "pitch": 0.17294048485119973,
+          "rotation": 0,
+          "target": "1-era-of-the-pandemic"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.974520341790253,
+          "pitch": 0.16991799675598074,
+          "title": "Past Pod",
+          "text": "A time where everything is \"normal\"--people are free to go about their activities and crowds are still a thing."
+        }
+      ]
+    },
+    {
+      "id": "1-era-of-the-pandemic",
+      "name": "Era of The Pandemic",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6505222819204475,
+        "pitch": 0.08388530532510252,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0962703900458424,
+          "pitch": 0.18460835664862252,
+          "rotation": 0,
+          "target": "2-the-future-is-here"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.2818356777919924,
+          "pitch": 0.17365008579977648,
+          "title": "Era of The Pandemic",
+          "text": "The pandemic begins and everyone is recommended to stay at home to slow down the spread of COVID-19."
+        }
+      ]
+    },
+    {
+      "id": "2-the-future-is-here",
+      "name": "The Future is Here",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.8791708040601627,
+        "pitch": 0.03816804696422871,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3104262740006014,
+          "pitch": 0.17597208319434543,
+          "rotation": 5.497787143782138,
+          "target": "3-the-different-phases"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.990127587277959,
+          "pitch": -0.0337748036108767,
+          "title": "The Future is Here",
+          "text": "People are busy climbing the ladder on going forward, but we forget to live in the moment. Right now, the time forces us to \"take a break,\" reflect on ourselves, and take care of other humankind."
+        }
+      ]
+    },
+    {
+      "id": "3-the-different-phases",
+      "name": "The Different Phases",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.930488900340106,
+        "pitch": 0.04155817208027557,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.080901299891325,
+          "pitch": 0.19544838160520683,
+          "rotation": 0,
+          "target": "4-social-distancing"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.080589587126597,
+          "pitch": 0.3564438605736875,
+          "title": "The Different Phases",
+          "text": "While going through the different phases of the pandemic, everyone must wear the mask and practice social distancing."
+        }
+      ]
+    },
+    {
+      "id": "4-social-distancing",
+      "name": "Social Distancing",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.743845128480726,
+        "pitch": 0.06254710071938163,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9048444745002104,
+          "pitch": 0.34753088058992887,
+          "rotation": 0,
+          "target": "5-reflection-pod"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.10214571464378,
+          "pitch": 0.33318202901284444,
+          "title": "Social Distancing",
+          "text": "Let's practice social distancing by maintaining at least 6 feet distance from others."
+        }
+      ]
+    },
+    {
+      "id": "5-reflection-pod",
+      "name": "Reflection Pod",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.4781870195462208,
+        "pitch": 0.022220473998341816,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.47023163777457,
+          "pitch": 0.4924747953860589,
+          "rotation": 6.283185307179586,
+          "target": "6-reflection-pod"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4515153173306228,
+          "pitch": -0.05813683917891055,
+          "title": "Reflection Pod",
+          "text": "Welcome to the Reflection Pod! Here is the space and time to reflect on ourselves."
+        }
+      ]
+    },
+    {
+      "id": "6-reflection-pod",
+      "name": "Reflection Pod",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.4581354561768851,
+        "pitch": -1.372459769961159,
+        "fov": 0.6521312022348882
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.679430758817624,
+          "pitch": -1.2102057804838697,
+          "title": "Reflection Pod",
+          "text": "In Reflection Pod, we hope you could take a step back, reflect, and find peace within yourself. Always remember that there is&nbsp;hope in every situation."
+        }
+      ]
+    },
+    {
+      "id": "7-the-new-normal",
+      "name": "The New Normal",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.4126365272219168,
+        "pitch": -0.012771996567213506,
+        "fov": 1.3687812585745385
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5390710889955663,
+          "pitch": 0.1925601067054128,
+          "rotation": 0,
+          "target": "8-introduction-of-the-future-pods"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7290747944037967,
+          "pitch": 0.1907263817474334,
+          "title": "The New Normal",
+          "text": "Keep on practicing social distancing and wearing mask as we return to normal activities."
+        }
+      ]
+    },
+    {
+      "id": "8-introduction-of-the-future-pods",
+      "name": "Introduction of The Future Pods",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5003016106939402,
+        "pitch": 0.01703689773712469,
+        "fov": 0.8355252772855437
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7315938116271905,
+          "pitch": 0.20571175591314805,
+          "rotation": 7.0685834705770345,
+          "target": "9-the-future-pods"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.726336753175696,
+          "pitch": 0.11313954420992367,
+          "title": "Introduction of The Future Pods",
+          "text": "The future is here! The Future Pod is a new platform for people to engage freely without physical contact."
+        }
+      ]
+    },
+    {
+      "id": "9-the-future-pods",
+      "name": "The Future Pods",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6827412201970011,
+        "pitch": 0.013224179397997915,
+        "fov": 1.0093480743896026
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2904085244618297,
+          "pitch": 0.29707758696052977,
+          "rotation": 0.7853981633974483,
+          "target": "10-the-future-pods"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.2910702577448667,
+          "pitch": 0.19026150730154257,
+          "title": "The Future Pods",
+          "text": "The Pods are a communication platform composed of two technological pods facing each other. This is a new design solution for socializing."
+        }
+      ]
+    },
+    {
+      "id": "10-the-future-pods",
+      "name": "The Future Pods",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.4895701438392734,
+        "pitch": 0.03972816283644498,
+        "fov": 1.1479073410113325
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9372188255758989,
+          "pitch": 0.4100648600855141,
+          "rotation": 0,
+          "target": "11-the-future-pods"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.0632381150798924,
+          "pitch": 0.385608650695314,
+          "title": "The Future Pods",
+          "text": "Please click the arrow to experience the pod."
+        }
+      ]
+    },
+    {
+      "id": "11-the-future-pods",
+      "name": "The Future Pods",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5938487183289958,
+        "pitch": 0.0035530020431373543,
+        "fov": 1.032447418818189
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.243971898875042,
+          "pitch": 0.24834959040116011,
+          "rotation": 7.0685834705770345,
+          "target": "12-connection-in-time"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.246156347524499,
+          "pitch": 0.1549010495978127,
+          "title": "The Future Pods",
+          "text": "In the Future Pods, people will be able to communicate using microphone-speaker installed in the pod. In addition for accessibility, the S<i>mart Screen</i>&nbsp;translates the conversation."
+        }
+      ]
+    },
+    {
+      "id": "12-connection-in-time",
+      "name": "CONNECTION IN TIME",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.9154156853260904,
+        "pitch": 0.0007999877481594808,
+        "fov": 1.2074389953102163
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 2.541498463497149,
+          "pitch": 0.1600413930004514,
+          "title": "CONNECTION IN TIME",
+          "text": "Thank you for participating in our virtual tour. We hope this reminds you to continuously connect with each other and with time again."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
